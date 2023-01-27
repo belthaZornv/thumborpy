2889fb1d91c987912a25d34e23992f16d74ac906
Julian <juliancamilleri1989@gmail.com>
2023-01-27 19:39:52 +0100

    Added support for filters

--- main.py
+++ main.py
@@ -1,56 +1,63 @@
+import asyncio
 import os
 
 from thumbor.context import Context
+from thumbor.filters import FiltersFactory
 from thumbor.server import get_config, configure_log, get_importer
 
 # use_environment to enable gathering of settings through env
 config = get_config(config_path=".", use_environment=False)
+
 configure_log(config, "DEBUG")
+
 importer = get_importer(config)
+
 context = Context(server=None, config=config, importer=importer)
+
+available_filters = FiltersFactory(context.modules.filters).filter_classes_map
 
 # read config
 uk_config = [
     {
-        "rotate": (50,),
-        "flip_horizontally": ()
+        "rotate": 50,
+        "brightness": 90
     },
     {
-        "rotate": (50,)
+        "rotate": 50,
     },
     {
-        "flip_vertically": (),
+        "blur": 10,
     },
-    {
-        "flip_horizontally": (),
-    }
 ]
 
 
-def get_filename(filename: str, filters_: dict, args: tuple):
-    filename += f"_{','.join(filters_.keys())}"
+def get_filename(filename: str, filters_: dict, value: int):
+    new_filename = f"{','.join(filters_.keys())}"
 
-    if args:
-        filename += f"_{','.join([str(arg) for arg in args])}"
+    if value:
+        new_filename += f"_{value}"
 
-    filename += ".jpg"
+    new_filename += filename
 
-    return filename
+    return new_filename
 
 
-def process_transformations():
-    # calling engine, although we'd need to call filters directly ideally (as custom logic is implemented there)
+async def process_transformations():
     for filename in os.listdir("./test_images"):
         for filters in uk_config:
             with open(f"./test_images/{filename}", "rb") as image:
                 context.modules.engine.load(image.read(), None)
 
                 # apply transformations
-                for filter_, args in filters.items():
-                    getattr(context.modules.engine, filter_)(*args)
+                for filter_, value in filters.items():
+                    parameters = f"{filter_}({value})" if value else f"{filter_}()"
 
-                with open(f"./produced_images/{get_filename(filename, filters, args)}", "wb") as new_image:
+                    Filter = available_filters.get(filter_)(params=parameters, context=context)
+
+                    await getattr(Filter, filter_)(value) if value else getattr(Filter, filter_)()
+
+                with open(f"./produced_images/{get_filename(filename, filters, value)}", "wb") as new_image:
                     new_image.write(context.modules.engine.read())
 
 
-process_transformations()
+asyncio.run(process_transformations())
